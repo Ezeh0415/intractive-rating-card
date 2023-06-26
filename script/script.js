8df5c93396884b1b646aa10ba962ea36cd0d907f
Ezeh Godwin <ezehgodwin3942@gmail.com>
2023-06-26 00:10:57 -0700

    adjustment

--- script/script.js
+++ script/script.js
@@ -5,23 +5,23 @@ let p4 = $(".p4");
 let p5 = $(".p5");
 
 p1.click(function(){
-   document.getElementById("demo").innerHTML = "you click 1 out of 5"
+   document.getElementById("demo").innerHTML = "You selected 1 out of 5"
 })
 
 p2.click(function(){
-   document.getElementById("demo").innerHTML = "you click 2 out of 5"
+   document.getElementById("demo").innerHTML = "You selected 2 out of 5"
 })
 
 p3.click(function(){
-   document.getElementById("demo").innerHTML = "you click 3 out of 5"
+   document.getElementById("demo").innerHTML = "You selected 3 out of 5"
 })
 
 p4.click(function(){
-   document.getElementById("demo").innerHTML = "you click 4 out of 5"
+   document.getElementById("demo").innerHTML = "You selected 4 out of 5"
 })
 
 p5.click(function(){
-   document.getElementById("demo").innerHTML = "you click 5 out of 5"
+   document.getElementById("demo").innerHTML = "You selected 5 out of 5"
 })
 
 $("button").click(function(){
